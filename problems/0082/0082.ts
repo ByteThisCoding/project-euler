af999c28437a0f820a19430faf941841a6702af6
brandonqdixon <brandonqdixon@gmail.com>
2021-11-07 18:46:23 -0500

    Problems Solved

---
+++ problems/0082/0082.ts
@@ -0,0 +1,87 @@
+import { AbstractSolution, RunSolution } from "../../utils/solution";
+import * as fs from "fs";
+
+@RunSolution
+export class Solution82 extends AbstractSolution {
+
+    getProblemName(): string {
+        return "Path Sum: Three Ways";
+    }
+
+    protected solve() {
+        return this.doSolve();
+    }
+
+    private doSolve(): number {
+        const matrix = this.readFile();
+        return this.findPathSum(matrix);
+    }
+
+    /**
+     * Use dynamic approach to construct from bottom right to top left
+     */
+    private findPathSum(matrix: number[][]): number {
+        const subSums = new Array(matrix.length)
+            .fill(null).map(() => {
+                return new Array(matrix[0].length)
+                    .fill(0);
+            });
+
+        const lastColInd = matrix[0].length - 1;
+
+        //populate solution matrix with rightmost column
+        for (let row=0; row<matrix.length; row++) {
+            subSums[row][lastColInd] = matrix[row][lastColInd];
+        }
+
+        //populate remaining columns
+        for (let col=lastColInd-1; col>=0; col--) {
+            //focus on rows in this column
+            for (let row=0; row<matrix.length; row++) {
+                //keep track of min so far
+                let minValue = matrix[row][col] + subSums[row][col+1];
+
+                //lookup until no spaces left
+                let rowFind;
+                let pastMin = false;
+                let searchValue = matrix[row][col];
+                for (rowFind=row-1; !pastMin && rowFind>=0; rowFind--) {
+                    searchValue += matrix[rowFind][col];
+                    pastMin = searchValue >= minValue;
+                    minValue = Math.min(
+                        minValue,
+                        searchValue + subSums[rowFind][col+1]
+                    );
+                }
+
+                //look down until no spaces left
+                pastMin = false;
+                searchValue = matrix[row][col];
+                for (rowFind=row+1; !pastMin && rowFind<matrix.length; rowFind++) {
+                    //duplicate of before (merge code?)
+                    searchValue += matrix[rowFind][col];
+                    pastMin = searchValue >= minValue;
+                    minValue = Math.min(
+                        minValue,
+                        searchValue + subSums[rowFind][col+1]
+                    );
+                }
+
+                subSums[row][col] = minValue;
+            }
+        }
+
+        //find smallest first col item
+        return subSums.reduce((min, row) => {
+            return row[0] < min ? row[0] : min;
+        }, Infinity);
+    }
+
+    private readFile(): number[][] {
+        const fileContents = fs.readFileSync(__dirname + "/matrix.txt", "utf-8");
+        return fileContents.split("\r\n").map(line => {
+            return line.split(",").map(item => parseInt(item));
+        });
+    }
+
+}
