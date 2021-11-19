34791f2eedd1da10688acb80d16ce60430fad01d
brandonqdixon <brandonqdixon@gmail.com>
2021-11-18 20:27:21 -0500

    spec fix

--- problems/0054/0054.spec.ts
+++ problems/0054/0054.spec.ts
@@ -4,7 +4,7 @@ describe("0054", () => {
 
     it("should give us coverage", async () => {
 
-        await new Solution54().run();
+        await new Solution54().run('console');
         //dummy to run test
         expect(true).toBe(true);
 
